4f5d7ba15673d50d3fb77da59f7db53c51e3e3db
capboyaryan <capboyaryan@gmail.com>
2022-09-09 10:29:57 +0530

    stuff

--- flask/test.py
+++ flask/test.py
@@ -1,5 +1,8 @@
-from flask import Flask,render_template
-
+import email
+import imp
+from lib2to3.pgen2.token import NEWLINE
+from flask import Flask,render_template,request,redirect
+import csv
 app = Flask(__name__)
 
 @app.route("/")
@@ -14,6 +17,27 @@ def work_route(page_name):
 # def hello_world_2(username):
 #     return render_template("name.html",name=username)
 
+def write_to_file(data):
+    with open("database.txt",mode='a') as database:
+        email=data["email"]
+        subject=data["subject"]
+        message=data["message"]
+        file=database.write(f"\n{email},{subject},{message}")
+
+def write_to_csv(data):
+    with open("database.csv",mode='a',newline='') as database2:
+        email=data["email"]
+        subject=data["subject"]
+        message=data["message"]
+        csv_writer=csv.writer(database2,delimiter=',',quotechar='"',quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email,subject,message])
+
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
-    return "form submitted!"
+    if request:
+        data=request.form.to_dict()
+        write_to_csv(data)
+        return redirect("/thankyou.html")
+    else:
+        return("Something went wrong!")
+
